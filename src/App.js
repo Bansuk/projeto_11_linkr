75357781aece24792bcadd6dfd0bafbebcb68736
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-09-16 13:40:58 -0300

    Testando deploy

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import UserContext from "./Context/UserContext";
 import { useState } from "react/cjs/react.development";
 
 export default function App() {
+    const [user, setUser] = useState({
+        token: "3582c710-71c0-46b4-8101-d57544fbc839",
+        user: {
+            id: 450,
+            email: "teste213@gmail.com",
+            username: "teste213",
+            avatar: "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/450/avatar",
+        },
+    });
+
     return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Switch>
-                <Route path="/timeline" exact>
-                    <Timeline />
-                </Route>
-                <Route path="/sign-up" exact>
-                    <SignUp />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <UserContext.Provider value={user}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Switch>
+                    <Route path="/timeline" exact>
+                        <Timeline />
+                    </Route>
+                    <Route path="/sign-up" exact setUser={setUser}>
+                        <SignUp />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
