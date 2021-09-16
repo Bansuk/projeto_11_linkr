3e52de00b708a379543082e64c626a4ec1bbd497
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-09-16 14:05:03 -0300

    Mockei o usuario sem useState

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import GlobalStyle from "./Styles/GlobalStyle";
 import Timeline from "./Components/Timeline";
 import SignUp from "./Components/signUp";
 import UserContext from "./Context/UserContext";
-import { useState } from "react/cjs/react.development";
 
 export default function App() {
+    const user = {
+        token: "3582c710-71c0-46b4-8101-d57544fbc839",
+        user: {
+            id: 450,
+            email: "teste213@gmail.com",
+            username: "teste213",
+            avatar: "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/450/avatar",
+        },
+    };
+
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={user}>
             <BrowserRouter>
                 <GlobalStyle />
                 <Switch>
                     <Route path="/timeline" exact>
                         <Timeline />
                     </Route>
-                    <Route path="/sign-up" exact>
+                    <Route path="/sign-up">
                         <SignUp />
                     </Route>
                 </Switch>
