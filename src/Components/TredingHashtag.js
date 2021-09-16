fb74dac5f53a218f0420ddaccd315dfed2f38cd2
jefranca <jean13franca@gmail.com>
2021-09-15 22:53:32 -0300

    Implemented Link in the hashtags

--- src/Components/TredingHashtag.js
+++ src/Components/TredingHashtag.js
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getHashtagsList } from "../Services/api.services"
+import HashtagList from "./HashtagList";
 export default function TrendingHashtag(){
-    const [hashtagsList, setHashtagsList]= useState([]);
+    const [hashtagsList, setHashtagsList]= useState("");
     console.log(hashtagsList)
 
+    
     useEffect(() => {
         getHashtagsList("2b19cbc1-856a-4d00-9666-2676d198c4bd")
         .then(response =>{
-            setHashtagsList(response.data)
+            setHashtagsList(response.data.hashtags)
         })
     },[])
-    return(
-        <CardTrendingHashtag>
-            <Trending>trending</Trending>
-            <Divider></Divider>
-        </CardTrendingHashtag>
-    )
+    if(hashtagsList !== ""){
+        return(
+            <CardTrendingHashtag>
+                <Trending>trending</Trending>
+                <Divider></Divider>
+                <ul>
+                    {hashtagsList.map((hashtags) => <HashtagList key={hashtags.id} hashtag={hashtags.name}/>)}
+                </ul>
+                
+            </CardTrendingHashtag>
+        )
+    }return ("")
+   
 }
+
+
 
 const CardTrendingHashtag = styled.div`
     border-radius: 16px;
@@ -26,6 +37,19 @@ const CardTrendingHashtag = styled.div`
 	background: #171717;
     display:flex;
     flex-direction: column;
+
+    ul{
+        margin: 22px 0 0 16px;
+        display:flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height:72%;
+    }
+
+    @media(max-width: 900px){
+        display:none;
+    
+
 `;
 const Divider = styled.div`
     width: 100%;
