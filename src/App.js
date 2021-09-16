f3ee18eb92401993742175afac6e14266bdad619
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-09-16 13:45:35 -0300

    Testando deploy

--- src/App.js
+++ src/App.js
@@ -6,25 +6,15 @@ import UserContext from "./Context/UserContext";
 import { useState } from "react/cjs/react.development";
 
 export default function App() {
-    const [user, setUser] = useState({
-        token: "3582c710-71c0-46b4-8101-d57544fbc839",
-        user: {
-            id: 450,
-            email: "teste213@gmail.com",
-            username: "teste213",
-            avatar: "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/450/avatar",
-        },
-    });
-
     return (
-        <UserContext.Provider value={user}>
+        <UserContext.Provider>
             <BrowserRouter>
                 <GlobalStyle />
                 <Switch>
                     <Route path="/timeline" exact>
                         <Timeline />
                     </Route>
-                    <Route path="/sign-up" exact setUser={setUser}>
+                    <Route path="/sign-up" exact>
                         <SignUp />
                     </Route>
                 </Switch>
--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -8,10 +8,10 @@ import UserContext from "../Context/UserContext";
 export default function Timeline() {
     const [statusMessage, setStatusMessage] = useState("Loading");
     const [postsList, setPostsList] = useState([]);
-    const { token } = useContext(UserContext);
+    //const { token } = useContext(UserContext);
 
     useEffect(() => {
-        getPostsList(token)
+        getPostsList("3582c710-71c0-46b4-8101-d57544fbc839")
             .then(res => {
                 setPostsList(res.data.posts);
                 if (postsList === [])
@@ -23,7 +23,7 @@ export default function Timeline() {
                     "Houve uma falha ao obter os posts, por favor atualize a página"
                 );
             });
-    }, [token, postsList]);
+    }, [postsList]);
 
     return (
         <Content>
