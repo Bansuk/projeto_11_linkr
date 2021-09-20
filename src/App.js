c7507b077cd30437816943239aa85b0c4e1f6275
Paulo <paulo_piotto@hotmail.com>
2021-09-20 15:45:09 -0300

    solving conflicts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, useHistory, Redirect } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import Timeline from "./components/Timeline";
 import SignUp from "./components/signUp";
 import Login from "./components/login";
 import UserContext from "./contexts/userContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TopBar from "./components/TopBar";
 import MyPosts from "./components/myPosts";
-import Hashtag from "./components/Hashtag";
 
 export default function App() {
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(() => {
+        let loggedUser = localStorage.getItem('user');
+        loggedUser = JSON.parse(loggedUser);
+        return loggedUser;
+    });
+
+    const isAuth = !!user;
+    
 
     return (
         <UserContext.Provider value={user}>
@@ -20,22 +26,20 @@ export default function App() {
                     <Route path="/sign-up" exact>
                         <SignUp />
                     </Route>
-                    <Route path="/" exact>
-                        <Login user={user} setUser={setUser} />
-                    </Route>
-                    <>
-                        <TopBar setUser={setUser} />
 
-                        <Route path="/timeline" exact>
-                            <Timeline />
-                        </Route>
-                        <Route path="/my-posts" exact >
-                            <MyPosts />
-                        </Route> 
-                        <Route path='/hashtag/:idHashtag' exact>
-                            <Hashtag />
-                        </Route>
-                    </>
+                    <Route path="/" exact>
+                        <Login user={user} setUser={setUser} isAuth={isAuth} />
+                    </Route>
+
+                    <Route path="/timeline" exact >
+                        <TopBar setUser={setUser} />
+                        <Timeline />                 
+                    </Route>
+
+                    <Route path="/my-posts" exact >
+                        <TopBar setUser={setUser} />
+                        <MyPosts />
+                    </Route> 
                 </Switch>
             </BrowserRouter>
         </UserContext.Provider>
