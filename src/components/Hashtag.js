5063ea2431024d84100e1171add0cb71f90a57c7
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-09-26 12:30:02 -0300

    Coloquei a funcao para evitar que repostBy fique undefined na pagina de um usuario e de uma hashtag

--- src/components/Hashtag.js
+++ src/components/Hashtag.js
@@ -1,42 +1,56 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../contexts/userContext";
 import { Content, Heading } from "../styles/MainPage";
-import { getHashtagPostsList } from '../services/api.services';
+import { getHashtagPostsList } from "../services/api.services";
 import Post from "./Post";
-import styled from 'styled-components';
-import TrendingHashtag from './Trending';
-export default function Hashtag(){
-const [statusMessage, setStatusMessage] = useState("Loading");
-const [postsList, setPostsList] = useState([]);
-const { token } = useContext(UserContext);
-const { idHashtag } = useParams();
+import styled from "styled-components";
+import TrendingHashtag from "./Trending";
+export default function Hashtag() {
+    const [statusMessage, setStatusMessage] = useState("Loading");
+    const [postsList, setPostsList] = useState([]);
+    const { token } = useContext(UserContext);
+    const { idHashtag } = useParams();
 
+    useEffect(() => {
+        getHashtagPostsList(token, idHashtag)
+            .then(res => {
+                setPostsList(res.data.posts);
+                setStatusMessage("Nenhum post encontrado");
+            })
+            .catch(err => {
+                setStatusMessage(
+                    "Houve uma falha ao obter os posts, por favor atualize a página"
+                );
+            });
+    }, [idHashtag, token]);
 
-useEffect(() => {
-    getHashtagPostsList(token, idHashtag)
-        .then(res => {
-            setPostsList(res.data.posts);
-            setStatusMessage("Nenhum post encontrado");
-        })
-        .catch(err => {
-            setStatusMessage(
-                "Houve uma falha ao obter os posts, por favor atualize a página"
-            );
-        });
-}, [idHashtag,token]);
+    function setRepostedBy(post) {
+        if (post.repostedBy) {
+            return {
+                repostUserId: post.repostedBy.id,
+                repostUsername: post.repostedBy.username,
+            };
+        } else return { repostUserId: "", repostUsername: "" };
+    }
 
-return (
-    <Content>
+    return (
+        <Content>
             <div className="posts">
                 <div>
                     {idHashtag.length <= 10 ? (
                         <Heading>#{idHashtag}</Heading>
-                    ):(
-                        <Heading>#{idHashtag.substring(0,7)}...</Heading>
+                    ) : (
+                        <Heading>#{idHashtag.substring(0, 7)}...</Heading>
                     )}
                     {postsList[0] ? (
-                        postsList.map(post => <Post key={post.id} post={post}></Post>)
+                        postsList.map(post => (
+                            <Post
+                                key={post.id}
+                                post={post}
+                                repostedBy={setRepostedBy(post)}
+                            ></Post>
+                        ))
                     ) : (
                         <Message>{statusMessage}</Message>
                     )}
@@ -44,7 +58,7 @@ return (
                 <TrendingHashtag />
             </div>
         </Content>
-);
+    );
 }
 
 const Message = styled.span`
